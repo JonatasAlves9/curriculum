72b589a66b4e3546dddb6103f22c9655c0ba33d1
Jônatas Alves <49556955+JonatasAlves9@users.noreply.github.com>
2024-02-24 23:27:21 -0300

    feat: remove '

--- src/app/aboutMe/page.tsx
+++ src/app/aboutMe/page.tsx
@@ -7,12 +7,13 @@ export default function Page() {
             <Section.Body>
                 <div className={"flex justify-between gap-3"}>
                     <div className={"p-6"}>
-                        <p className={"text-gray-400 font-normal text-pretty"}>Hey, there 👋🏻 hi I'm Benjamin, a Software developer and Data scientist with over 8+ years of
+                        <p className={"text-gray-400 font-normal text-pretty"}>Hey, there 👋🏻 hi Im Benjamin, a Software
+                            developer and Data scientist with over 8+ years of
                             experience, specialising in Java and React. Also I proficient at using tools and programming
                             languages such as Python or SQL to manipulate and analyze data.</p>
                     </div>
                     <div className={"border border-gray-700 border-t-0 p-6"}>
-                        <p>Hey, there hi I'm Benjamin, a Software developer and Data scientist with over 8+ years of
+                        <p>Hey, there hi Im Benjamin, a Software developer and Data scientist with over 8+ years of
                             experience, specialising in Java and React. Also I proficient at using tools and programming
                             languages such as Python or SQL to manipulate and analyze data.</p>
                     </div>
